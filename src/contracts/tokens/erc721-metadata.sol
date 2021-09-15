dba4c3b28855b50e2f35b117e61331f3f43861c6
sda-p <76849391+sda-p@users.noreply.github.com>
2021-09-15 11:02:10 +0100

    Removed Token URIs

--- src/contracts/tokens/erc721-metadata.sol
+++ src/contracts/tokens/erc721-metadata.sol
@@ -26,15 +26,4 @@ interface ERC721Metadata
     view
     returns (string memory _symbol);
 
-  /**
-   * @dev Returns a distinct Uniform Resource Identifier (URI) for a given asset. It Throws if
-   * `_tokenId` is not a valid NFT. URIs are defined in RFC3986. The URI may point to a JSON file
-   * that conforms to the "ERC721 Metadata JSON Schema".
-   * @return URI of _tokenId.
-   */
-  function tokenURI(uint256 _tokenId)
-    external
-    view
-    returns (string memory);
-
 }
